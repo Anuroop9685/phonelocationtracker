f7573536d1610ce37c7c7b75f9b47e4dff8a3bb0
Anuroop Jayakumar <anuroopjayakumar73@gmail.com>
2022-05-01 08:15:48 +0530

    Phone number location tracker completed

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import phonenumbers
 from test import*
 
 from phonenumbers import geocoder
+ch_number=phonenumbers.parse(number, "CH")
+print(geocoder.description_for_number(ch_number, "en"))
+
+from phonenumbers import carrier
+service_number = phonenumbers.parse(number, "RO")
+print(carrier.name_for_number(service_number , "en"))
